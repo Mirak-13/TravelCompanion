e19fc7841d483e52bed8848a0a8137b35af75ce8
Карим Чагаров <karim@MacBook-Air-Karim.local>
2025-02-19 21:03:35 +0400

    разделил логику добавив models и переименовал все функции

---
+++ places_api/config.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
+url = "https://api.foursquare.com/v3/places/search"
+
+headers = {
+    "accept": "application/json",
+    "Accept-Language": "ru",
+    "Authorization": API_KEY,
+}
+
+
